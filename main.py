cd2dd2bd6302906ae3dec8224755e9c59a99202f
printnewcode <zenit200803@mail.ru>
2024-03-19 14:12:29 +0300

    update

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-from config import app
+from config import app, Flask, render_template
 from random import randint
+from models import Flat
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("flats.html")
 
 
 @app.route("/cats/")
 def cats():
     cat_name = "Barsic"
     return render_template("cats.html", cat_name=cat_name)
+
+
+@app.route("/flats/")
+def flats_view():
+    flats = Flat.query.all()
+    return render_template("flats.html", flats=flats)
+
+
+@app.route("/flat/<flat_id>")
+def flat_view(flat_id):
+    flat = Flat.query.get(id=flat_id)
+    return render_template("flat.html", flat=flat)
 
 
 @app.route("/info/<name>/<age>")
--- models.py
+++ models.py
@@ -4,6 +4,7 @@ from config import app, db
 
 
 class Human(db.Model):
+    """Модель описания квартир"""
     id = db.Column(db.Integer, primary_key=True)
     name: str = db.Column(db.String(30), index=True, unique=False)
     sec_name: str = db.Column(db.String(50), index=True, unique=False)
@@ -12,4 +13,16 @@ class Human(db.Model):
     age: int = db.Column(db.String(3), index=True, unique=False)
     country: str = db.Column(db.String(50), index=True, unique=False)
     bio: str = db.Column(db.String(10000), index=True, unique=False)
-    datetime: str = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+class Flat(db.Model):
+    """Модель описания квартир."""
+    id = db.Column(db.Integer, primary_key=True)
+    period: int = db.Column(db.String(10), unique=False)
+    bathroom: int = db.Column(db.String(2), unique=False)
+    floor: int = db.Column(db.String(3), unique=False)
+    Square: int = db.Column(db.String(4), unique=False)
+    city: str = db.Column(db.String(30), unique=False)
+    rooms: str = db.Column(db.String(50),  unique=False)
+    price: int = db.Column(db.String(10), unique=False)
+    area: str = db.Column(db.String(50), unique=False)
